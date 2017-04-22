be84204665ebb117cba650b3d59d17e9a33d39f0
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2017-04-22 17:39:55 +0530

    Camera: Improve camera movement
    The camera now moves only when the player reaches
    the edges of the screen. The camera speed is 5
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- SmallWorld/SmallWorld/Assets/Scripts/Camera/CameraControls.cs
+++ SmallWorld/SmallWorld/Assets/Scripts/Camera/CameraControls.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(Camera))]
+[RequireComponent(typeof(SmoothFollow))]
 public class CameraControls : MonoBehaviour {
 
     Camera cam;
-
     float t = 0;
+
+    //int doOnce = 0;
 	void Start () {
         cam = GetComponent<Camera>();
 	}
@@ -16,5 +18,20 @@ public class CameraControls : MonoBehaviour {
         cam.orthographicSize = Mathf.Lerp(0, 10, t);
 
         t += 0.5f * Time.deltaTime;
+
+        /*
+        if (LevelManager.currentLevel >= 2 && doOnce == 0) {
+            doOnce++;
+            StartCoroutine(FollowPlayer());
+        }
+        */
 	}
+
+    /*
+    IEnumerator FollowPlayer()
+    {
+        yield return new WaitForSeconds(2.0f);
+        cam.GetComponent<SmoothFollow>().enabled = true;
+    }
+    */
 }
---
+++ SmallWorld/SmallWorld/Assets/Scripts/Camera/CameraEdgeFollow.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CameraEdgeFollow : MonoBehaviour {
+
+    Camera cam;
+    float speed = 3.0f;
+
+    GameObject thePlayer;
+    /*
+    int boundary = 50;
+    float screenWidth, screenHeight;
+    */
+
+	void Start () {
+        cam = GetComponent<Camera>();
+        thePlayer = GameObject.Find("Player");
+        if (thePlayer == null) {
+            Debug.LogError("ERROR: No Player found");
+        }
+        /*
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        */
+	}
+	
+
+	void Update () {
+        /*
+        float xPos = thePlayer.transform.position.x;
+        float yPos = thePlayer.transform.position.y;
+        if (xPos > screenWidth) { }
+        */
+        Vector3 viewPos = cam.WorldToViewportPoint(thePlayer.transform.position);
+        //print(viewPos.x+","+viewPos.y);
+        if (viewPos.x <= 0.2)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        else if (viewPos.x >= 0.8) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        if (viewPos.y <= 0.2)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        else if (viewPos.y >= 0.8) {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+	}
+}
